e585e6a5120a7934517cb4e3fc86351e19543fa2
David Wolfe <wolfe@sheepdoginc.ca>
2012-01-12 15:11:14 -0400

    Removed junk forms.py

--- src/forms.py
+++
@@ -1,6 +0,0 @@
-class submit_access_form(forms.Form):
-    selection = forms.ChoiceField(choices=[])
-
-    def __init__(self, list_of_orgs, *args, **kwargs):
-        super(submit_access_form,self).__init__(*args, **kwargs)
-        self.fields['selection'].choices = [(org.pk,org.name) for org in list_of_orgs]
