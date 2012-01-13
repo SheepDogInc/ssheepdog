5f6f8df7383e65d10e4493b729e366563b2090ca
Travis Macmillan <travis.macmillan@dal.ca>
2012-01-13 14:44:01 -0400

    ssh working great

--- src/ssheepdog/management/commands/update_ssh.py
+++ src/ssheepdog/management/commands/update_ssh.py
@@ -2,12 +2,6 @@ from django.core.management.base import BaseCommand, CommandError
 from ssheepdog.sync import test_sync
 
 class Command(BaseCommand):
-    args = '<poll_id poll_id ...>'
-    help = 'Closes the specified poll for voting'
-
     def handle(self, *args, **options):
         test_sync()
-        self.stdout.write("*********** I AM RUNNING ****************\n")
-        print "Just curious what print does" # Probably not advised
-        raise CommandError("But I was not finished yet")
 
--- src/ssheepdog/sync.py
+++ src/ssheepdog/sync.py
@@ -6,8 +6,6 @@ def test_sync():
     users = User.objects.select_related('_profile_cache')
     logins = Login.objects.all()
     authorized_keys = []
-    for user in users:
-        user.ssh_key = user.get_profile().ssh_key
     for login in logins:
         for user in users:
             if user in login.users.all():
@@ -15,6 +13,5 @@ def test_sync():
                 env.host_string = "%s@%s:%d" % (login.username,
                         (m.ip or m.hostname),
                         m.port)
-                authorized_keys.append(str(user.ssh_key))
-        print str(env.host_string)
+                authorized_keys.append(str(user.get_profile().ssh_key))
         run('echo "%s" > ~/.ssh/authorized_keys' % "\n".join(authorized_keys))
