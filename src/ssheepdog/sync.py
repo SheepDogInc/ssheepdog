c791ad1a19470351096c970a0365dff0bc4d6fa3
David Wolfe <wolfe@sheepdoginc.ca>
2012-01-13 11:12:30 -0400

    Completed test_sync method

--- src/ssheepdog/sync.py
+++ src/ssheepdog/sync.py
@@ -1,20 +1,16 @@
 from django.contrib.auth.models import User
-from django.template import Context, loader
-from src import ssheepdog
-from ssheepdog.models import UserProfile, Machine, Login, Client
+from ssheepdog.models import Login
+from fabric.api import env, sudo
 
-
-class Sync(object):
-    def __init__(self, path=None):
-        users = User.objects.select_related('_profile_cache')
-        logins = login.objects.all()
-        authorized_keys = []
+def test_sync():
+    users = User.objects.select_related('_profile_cache')
+    logins = Login.objects.all()
+    authorized_keys = []
+    for user in users:
+        user.ssh_key = user.get_profile().ssh_key
+    for login in logins:
         for user in users:
-            user.ssh_key = user.get_profile().ssh_key
-        for login in logins:
-            for user in users:
-                if user in login.users.all():
-                    env.host_string = login.username+"@"+login.machine     
-                    authorized_keys.append(user.ssh_key)
-        sudo('echo "%s" > ~/.ssh/authorized_keys' %
-            authorized_keys)
+            if user in login.users.all():
+                env.host_string = login.username+"@" + str(login.machine)
+                authorized_keys.append(str(user.ssh_key))
+        sudo ('echo "%s" > ~/.ssh/authorized_keys_test' % "\n".join(authorized_keys))
