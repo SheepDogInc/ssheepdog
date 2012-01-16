099b4c58e70c835dbf86e6331b42c66a94109f3f
Travis Macmillan <travis.macmillan@dal.ca>
2012-01-16 11:09:44 -0400

    sync appears to be working

--- src/ssheepdog/sync.py
+++ src/ssheepdog/sync.py
@@ -5,13 +5,14 @@ from fabric.api import env, sudo, run
 def test_sync():
     users = User.objects.select_related('_profile_cache')
     logins = Login.objects.all()
-    authorized_keys = []
     for login in logins:
-        for user in users:
-            if user in login.users.all():
-                m = login.machine
-                env.host_string = "%s@%s:%d" % (login.username,
+        authorized_keys = []
+        if login.is_active:
+            for user in users:
+                if user in login.users.all().filter(is_active = True):
+                    m = login.machine
+                    env.host_string = "%s@%s:%d" % (login.username,
                         (m.ip or m.hostname),
                         m.port)
-                authorized_keys.append(str(user.get_profile().ssh_key))
-        run('echo "%s" > ~/.ssh/authorized_keys' % "\n".join(authorized_keys))
+                    authorized_keys.append(user.get_profile().ssh_key)
+            run('echo "%s" > ~/.ssh/authorized_keys' % "\n".join(authorized_keys))
