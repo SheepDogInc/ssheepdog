98e4d1e565f0bc95b0a300238b6f4527dd56e0b7
Travis Macmillan <travis.macmillan@dal.ca>
2012-01-16 16:45:08 -0400

    added the application ssh key

--- src/ssheepdog/sync.py
+++ src/ssheepdog/sync.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.models import User
 from ssheepdog.models import Login
 from fabric.api import env, sudo, run
+import os
+import settings
 
+root = getattr(settings, 'PROJECT_ROOT', None)
+keys_dir = os.path.join(root, '../deploy/keys')
 def test_sync():
     users = User.objects.select_related('_profile_cache')
     logins = Login.objects.all()
     for login in logins:
         authorized_keys = []
+        env.key_filename = os.path.join(keys_dir, 'application')
+        authorized_keys.append(read_file(os.path.join(keys_dir,'application.pub')))
         if login.is_active:
             for user in users:
                 if user in login.users.all().filter(is_active = True):
@@ -15,4 +21,12 @@ def test_sync():
                         (m.ip or m.hostname),
                         m.port)
                     authorized_keys.append(user.get_profile().ssh_key)
-            run('echo "%s" > ~/.ssh/authorized_keys' % "\n".join(authorized_keys))
+            run('echo "%s" > ~/.ssh/authorized_keys_test' % "\n".join(authorized_keys))
+def read_file(filename):
+       """
+       Read data from a file and return it
+       """
+       f = open(filename)
+       data = f.read()
+       f.close()
+       return data
