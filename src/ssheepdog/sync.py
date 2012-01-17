d11384d9d897bd672dcdd64e07f052c99d256fc0
Travis Macmillan <travis.macmillan@dal.ca>
2012-01-17 13:33:48 -0400

    sync working

--- src/ssheepdog/sync.py
+++ src/ssheepdog/sync.py
@@ -15,13 +15,13 @@ def read_file(filename):
 def test_sync():
     for login in Login.objects.all():
         authorized_keys = [read_file(os.path.join(keys_dir, 'application.pub'))]
-        if login.is_active:
-            m = login.machine
-            env.abort_on_prompts = True
-            env.key_filename = os.path.join(keys_dir, 'application')
-            env.host_string = "%s@%s:%d" % (login.username,
-                                            (m.ip or m.hostname),
-                                            m.port)
+        m = login.machine
+        env.abort_on_prompts = True
+        env.key_filename = os.path.join(keys_dir, 'application')
+        env.host_string = "%s@%s:%d" % (login.username,
+                (m.ip or m.hostname),
+                m.port)    
+        if login.is_active and login.machine.is_active:
             for user in (login.users
                          .filter(is_active = True)
                          .select_related('_profile_cache')):
