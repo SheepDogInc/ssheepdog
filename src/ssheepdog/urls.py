c4ae3973bf8ee71f114197318a8ab7a0eb127997
David Wolfe <wolfe@sheepdoginc.ca>
2012-01-26 12:40:43 -0400

    Added basic login view

--- src/ssheepdog/urls.py
+++ src/ssheepdog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
 	url(r'^$', 'ssheepdog.views.view_access_summary'),
@@ -6,4 +7,6 @@ urlpatterns = patterns('',
 	url(r'^sync_keys/$', 'ssheepdog.views.sync_keys'),
 	url(r'^user/(?P<id>[0-9]+)/$', 'ssheepdog.views.user_admin_view'),	
 	url(r'^login/(?P<id>[0-9]+)/$','ssheepdog.views.login_admin_view'),
+    url(r'^login/$', login, {}, name='login'),
+    url(r'^logout/$', logout, {}, name='logout'),
 )
