f50d20ceb0e252c5f5ac2ca796d8559718c9f287
Honza Pokorny <me@honza.ca>
2012-01-28 20:20:25 -0400

    Initial pass

---
+++ src/ssheepdog/static/js/jquery-client.js
@@ -0,0 +1,32 @@
+(function($) {
+
+  var modal;
+
+  $(function() {
+
+    modal = $('#modal');
+    modal.modal();
+
+    $('.primary', modal).click(function() {
+      modal.modal('hide');
+    });
+
+    $('a.login').click(function() {
+      $.get(this.href, {}, function(data) {
+        $('.modal-body', modal).html(data);
+        modal.modal('show');
+      });
+      return false;
+    });
+
+    $('a.user').click(function() {
+      $.get(this.href, {}, function(data) {
+        $('.modal-body', modal).html(data);
+        modal.modal('show');
+      });
+      return false;
+    });
+
+  });
+
+})(jQuery);
