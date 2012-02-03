6d2f24d824b61c63b4f5abda6700cf849a0873d6
Honza Pokorny <me@honza.ca>
2012-02-03 11:36:34 -0400

    Packaging basics

---
+++ pluggable/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+description = 'ssheepdog'
+long_desc = description
+
+setup(
+    name='ssheepdog',
+    version='0.0.1',
+    url='https://github.com/SheepDogInc/ssheepdog',
+    install_requires=[
+        'django-celery',
+        'django-kombu',
+    ],
+    description=description,
+    long_description=long_desc,
+    author='SheepDogInc',
+    author_email='info@sheepdoginc.ca',
+    packages=['ssheepdog']
+)
