54e6904ed0a3794fa9095ca07e0f996cae38313c
David Wolfe <wolfe@sheepdoginc.ca>
2012-02-05 16:09:28 -0400

    Redisplay of modal form on failed submit

--- ssheepdog/static/js/jquery-client.js
+++ ssheepdog/static/js/jquery-client.js
@@ -12,11 +12,27 @@
 
     $('a.open-modal-form').click(function() {
       $.get(this.href, {}, function(data) {
-        $('.modal-body', modal).html(data);
+        $('div.modal-body', modal).html(data);
         modal.modal('show');
       });
       return false;
     });
+
+    $('div.modal-body').delegate('input.btn', 'click', (function(event) {
+      var $form = $(event.target).closest('form');
+      $.post($form.attr('action'),
+             $form.serialize(),
+             function(data) {
+               if (data.match("<html")) // Full page returned - KLUGE ALERT - just redirect to /
+                 window.location = "/";
+               else { // Partial page returned; assume it's a replacement form
+                 $('div.modal-body', modal).html(data);
+               }
+               return false;
+             });
+      return false;
+    }));
+
 
     $('.twipsy').tooltip();
 
