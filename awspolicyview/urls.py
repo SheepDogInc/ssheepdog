390d02d520372c84c7e3f774fa2bdb6d5c3f849e
Rob Butler <robert.butler@sheepdog.com>
2013-08-07 13:07:41 -0300

    Added modals to display policy details

--- awspolicyview/urls.py
+++ awspolicyview/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from awspolicyview.views import ListAWSPoliciesView
 
 aws_patterns = patterns('awspolicyview.views',
-    url(r'^$', ListAWSPoliciesView.as_view(), name='policy_view'),
+    url(r'^$', ListAWSPoliciesView.as_view(), name='policy_list'),
 )
--- awspolicyview/views.py
+++ awspolicyview/views.py
@@ -1,5 +1,9 @@
 import boto
+import urllib2
+import json
+
 from django.views.generic import TemplateView
+from django.utils.safestring import SafeString
 
 
 class ListAWSPoliciesView(TemplateView):
@@ -7,34 +11,37 @@ class ListAWSPoliciesView(TemplateView):
 
     def get_context_data(self, **kwargs):
         def dry_helper(current_list, key, value):
-            current_list[key] += value \
-                    if not current_list[key] \
-                    else ', %s' % value
+            current_list[key].append(value)
 
         context = super(ListAWSPoliciesView, self).get_context_data(**kwargs)
         
         user_list = {}
         role_list = {}
         group_list = {}
+        policy_list = {}
 
         iam = boto.connect_iam()
         for user in iam.get_all_users().list_users_result.users:
-            user_list[user.user_name] = ''
+            user_list[user.user_name] = []
             for policy in iam.get_all_user_policies(user.user_name).list_user_policies_result.policy_names:
                 dry_helper(user_list, user.user_name, policy)
+                policy_list[policy] = urllib2.unquote(iam.get_user_policy(user.user_name, policy).policy_document)
 
         for role in iam.list_roles().list_roles_result.roles:
-            role_list[role.role_name] = ''
+            role_list[role.role_name] = []
             for policy in iam.list_role_policies(role.role_name).policy_names:
                 dry_helper(role_list, role.role_name, policy)
+                policy_list[policy] = urllib2.unquote(iam.get_role_policy(role.role_name, policy).policy_document)
 
         for group in iam.get_all_groups().list_groups_result.groups:
-            group_list[group.group_name] = ''
+            group_list[group.group_name] = []
             for policy in iam.get_all_group_policies(group.group_name).policy_names:
                 dry_helper(group_list, group.group_name, policy)
+                policy_list[policy] = urllib2.unquote(iam.get_group_policy(group.group_name, policy).policy_document)
 
         context.update({'user_policies': user_list,
                         'role_policies': role_list,
-                        'group_policies': group_list
+                        'group_policies': group_list,
+                        'policies': SafeString(json.dumps(policy_list)),
                         })
         return context
